341128ed589093a56fa5aab65d4fbbbc3051636a
modenadev <ruanscorpionn@gmail.com>
2025-12-02 13:48:33 -0300

    Configurações do projeto

--- config/RabbitMQConfig.java
+++ config/RabbitMQConfig.java
@@ -1,3 +1,52 @@
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
-    
+
+    @Value("${app.rabbitmq.queue}")
+    private String queueName;
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchangeName;
+
+    @Value("${app.rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public Queue pedidoQueue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public DirectExchange pedidoExchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding pedidoBinding(Queue pedidoQueue, DirectExchange pedidoExchange) {
+        return BindingBuilder.bind(pedidoQueue).to(pedidoExchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jacksonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter jacksonMessageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jacksonMessageConverter);
+        return template;
+    }
 }
