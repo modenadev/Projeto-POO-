0492048605da15b8dc3b6701033afeaa4793ae7c
modenadev <ruanscorpionn@gmail.com>
2025-12-02 14:34:14 -0300

    Código dos controllers!

--- src/main/java/com/example/controller/NotificacaoController.java
+++ src/main/java/com/example/controller/NotificacaoController.java
@@ -1,5 +1,24 @@
 package com.example.controller;
 
+import com.example.entity.Notificacao;
+import com.example.service.NotificacaoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notificacoes")
 public class NotificacaoController {
-    
+
+    private final NotificacaoService notificacaoService;
+
+    public NotificacaoController(NotificacaoService notificacaoService) {
+        this.notificacaoService = notificacaoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Notificacao>> listarTodas() {
+        return ResponseEntity.ok(notificacaoService.listarTodas());
+    }
 }
